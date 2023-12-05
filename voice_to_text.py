70d0d67df4d034a321e4c5a8e7019805cf49cc47
rty123444 <dearchencc@gmail.com>
2023-12-06 01:17:13 +0800

    Fix print statements and update voice_to_text.py

--- voice_to_text.py
+++ voice_to_text.py
@@ -1,29 +1,41 @@
 import sounddevice as sd
 import numpy as np
 import speech_recognition as sr
+import wave
+import io
 
-def record_from_mic(duration=5, sample_rate=44100):
-    print(f"錄製 {duration} 秒的音頻...")
+def record_audio(duration=5, sample_rate=44100):
+    print(f"Recording for {duration} seconds...")
     recording = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16')
-    sd.wait()  # 等待錄製完成
-    return np.array(recording).flatten()
+    sd.wait()
+    return recording
+
+def save_audio_to_wav(recording, sample_rate=44100):
+    wav_io = io.BytesIO()
+    with wave.open(wav_io, 'wb') as wav_file:
+        wav_file.setnchannels(1)
+        wav_file.setsampwidth(2)
+        wav_file.setframerate(sample_rate)
+        wav_file.writeframes(recording.tobytes())
+    wav_io.seek(0)
+    return wav_io
 
 def voice_to_text(duration=5):
     recognizer = sr.Recognizer()
-    audio_data = record_from_mic(duration)  # 錄製指定秒數的音頻
+    audio_data = record_audio(duration)
+    wav_audio = save_audio_to_wav(audio_data)
 
-    # 將 numpy 數組音頻轉換為音頻數據
-    audio = sr.AudioData(np.array(audio_data).tobytes(), 44100, 2)
+    with sr.AudioFile(wav_audio) as source:
+        audio = recognizer.record(source)
 
     try:
-        # 使用 Google 的 Web Speech API 進行識別
         text = recognizer.recognize_google(audio, language="zh-TW")
         return text
     except sr.UnknownValueError:
-        return "無法識別語音"
+        return "无法识别语音"
     except sr.RequestError as e:
-        return f"無法從 Google Web Speech API 獲取結果; {e}"
+        return f"无法从 Google Web Speech API 获取结果; {e}"
 
 if __name__ == "__main__":
     text = voice_to_text(duration=5)
-    print("您說的話是： " + text)
+    print("您说的话是： " + text)
